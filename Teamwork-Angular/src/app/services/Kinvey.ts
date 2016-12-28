12dd7ecc27bdd4a995aac7a78ed452c771a52f35
Sandartino <sandartino@yahoo.com>
2016-12-28 12:01:34 +0200

    z

--- Teamwork-Angular/src/app/services/Kinvey.ts
+++ Teamwork-Angular/src/app/services/Kinvey.ts
@@ -1,7 +1,7 @@
 export class Kinvey {
-    public static readonly baseUrl: string = 'https://baas.kinvey.com/';
-    public static readonly appKey: string = 'kid_H1tVQj04e';
-    public static readonly appSecret: string = 'add9c46a97a84508a46221334f8d224a';
+    public static  baseUrl: string = 'https://baas.kinvey.com/';
+    public static  appKey: string = 'kid_H1tVQj04e';
+    public static  appSecret: string = 'add9c46a97a84508a46221334f8d224a';
 
     public static appAuthHeaders() {
     return "Basic " + btoa(Kinvey.appKey + ":" + Kinvey.appSecret)
